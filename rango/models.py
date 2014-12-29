02f12ce50305b73afe02b06f0908e5404d5a57d6
Krupski Grzegorz <krupskig@gmail.com>
2014-12-29 20:46:11 +0100

    j

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -16,3 +18,4 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,65 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
+from rango.models import Page
+from rango.forms import CategoryForm
+
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "I am bold font from context"}
+    category_list = Category.objects.order_by('-like')[:5]    
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'pages': page_list}
+    context_dict = {'categories': category_list}
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
     return render_to_response('rango/index.html', context_dict, context)
+
 def about(request):
     context = RequestContext(request)
     context_dict = {'boldmessage': "I am bold font from context"}
     return render_to_response('rango/about.html', context_dict, context)
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+    category_name = category_name_url.replace('_', ' ')
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render_to_response('rango/category.html', context_dict, context)
+
+def add_category(request):
+    # Get the context from the request.
+    context = RequestContext(request)
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            # Save the new category to the database.
+            form.save(commit=True)
+
+            # Now call the index() view.
+            # The user will be shown the homepage.
+            return index(request)
+        else:
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
+    else:
+        # If the request was not a POST, display the form to enter details.
+        form = CategoryForm()
+
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('rango/add_category.html', {'form': form}, context)
+
